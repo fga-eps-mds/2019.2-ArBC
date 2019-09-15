7d73d210027844ab954c03034fc20e7fee228216
Igor Batista Paiva <igorbatistapaiva@outlook.com>
2019-09-14 22:45:23 -0300

    Integration with user camera

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,116 @@
 <template>
-  <div class="home">
-    <img alt="Yoda" src="../assets/yoda.svg"/>
-    <HelloWorld msg="Patience you must have my young Padawan."/>
+  <div>
+    <h1>ArBC</h1>
+
+    <button
+      v-if="!cameraOpened"
+      @click="openCamera()"
+      class="camera-btn">
+      ABRIR CÂMERA
+    </button>
+
+    <button
+      v-else
+      @click="closeCamera()"
+      class="camera-btn">
+      FECHAR CÂMERA
+    </button>
+
+    <h4 v-if="message">{{ message }}</h4>
+
+    <video ref="video" class="video" />
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<style>
+  .video {
+    margin: 0 auto;
+    width: 100%;
+  }
+  .camera-btn {
+    width: 160px;
+    height: 40px;
+    margin-top: 20px;
+    margin-bottom: 30px;
+    border-radius: 10px;
+  }
+</style>
 
-@Component({
-  components: {
-    HelloWorld,
+<script lang="ts">
+export default {
+  name: 'Home',
+  data() {
+    return {
+      message: null,
+      mediaStream: null,
+      cameraOpened: false,
+      mediaConstraints: {
+        video: {
+          facingMode: 'environment',
+        },
+      },
+    };
   },
-})
-export default class Home extends Vue {}
+  destroyed() {
+    this.message = null;
+  },
+  methods: {
+    handleMediaDevicesUndefined() {
+      const getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      if (!getUserMedia) {
+        this.message = 'Não é possível acesar a câmera através desse navegador...';
+      }
+
+      getUserMedia(this.mediaConstraints)
+        .then(this.handleSucessOnUserMedia)
+        .catch(this.handleErrorOnUserMedia)
+    },
+    handleSucessOnUserMedia(mediaStream) {
+      this.mediaStream = mediaStream;
+
+      let video = this.$refs.video;
+
+      if ("srcObject" in video) {
+        video.srcObject = mediaStream;
+      } else {
+        video.src = window.URL.createObjectURL(mediaStream);
+      }
+
+      video.onloadedmetadata = () => {
+        video.play();
+      }
+
+      this.cameraOpened = true;
+    },
+    handleErrorOnUserMedia(error) {
+      switch (error.name) {
+        case 'NotAllowedError':
+          this.message = 'A permissão para uso da câmera não foi concebida.';
+
+          break;
+        case 'NotReadableError':
+          this.message = 'Houve algum erro no aparelho...';
+
+          break;
+        default:
+          this.message = 'Ocorreu algum erro desconhecido... Tente novamente.';
+      }
+    },
+    openCamera() {
+      if ('mediaDevices' in navigator) {
+        navigator.mediaDevices.getUserMedia(this.mediaConstraints)
+          .then(mediaStream => this.handleSucessOnUserMedia(mediaStream))
+          .catch(error => this.handleErrorOnUserMedia(error))
+      } else {
+        this.handleMediaDevicesUndefined();
+      }
+    },
+    closeCamera() {
+      this.mediaStream.getTracks().forEach(track => track.stop());
+
+      this.$router.go();
+    },
+  },
+};
 </script>
