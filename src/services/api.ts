0522e71863b2ffe2ff455d60cab3f00d876ea3f9
igor-paiva <igorbatistapaiva@outlook.com>
2019-10-01 16:15:54 -0300

    Create api service and add environment variables for development

---
+++ src/services/api.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_URL = process.env.VUE_APP_API_URL;
+
+const CONFIG = {
+  timeout: 5000,
+  baseURL: API_URL,
+  headers: {
+    Accept: 'application/json',
+  }
+};
+
+export default {
+  get: (path: string) => axios.get(path, CONFIG)
+    .then((response: any) => response.data)
+};
