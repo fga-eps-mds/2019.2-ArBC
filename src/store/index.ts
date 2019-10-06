94b3f759c9b2e7064cd87c680264e4da2e08e5b8
igor-paiva <igorbatistapaiva@outlook.com>
2019-10-05 21:50:25 -0300

    Add word module to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Vuex, { StoreOptions } from 'vuex';
 import { RootState } from './types';
 import { letter } from './letter';
+import { word } from './word';
 
 Vue.use(Vuex);
 
 const store: StoreOptions<RootState> = {
   state: {},
   modules: {
+    word,
     letter,
   },
 };
---
+++ src/store/word/types.ts
@@ -0,0 +1,3 @@
+export interface WordState {
+  word: object;
+}
