0fd1b4212a56c58809f3881ebfc0a3e0d04e0839
Sergio Cipriano <sergiosacj@hotmail.com.br>
2019-10-23 15:33:40 -0300

    Added new routes
    Co-authored-by: Marcelo Victor <marcelovictorg2@gmail.com>

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    <h1>ArBC</h1>
+    <h1><a href="https://jlucassr.github.io/ArBC-Pages/">Sobre</a></h1>
+    <img alt="Arara" src="../assets/arara.svg"/>
+  </div>
+</template>
