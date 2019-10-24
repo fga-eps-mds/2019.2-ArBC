32fc4c0139d8184da5e13fba630e5b858c23a823
marcelog5 <marcelovictorg2@gmail.com>
2019-10-23 22:02:13 -0300

    sass instaled
    Co-authored-by: Sergio Cipriano <sergiosacj@hotmail.com.br>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="home">
     <h1>ArBC</h1>
-    <h1><a href="https://jlucassr.github.io/ArBC-Pages/">Sobre</a></h1>
-    <h1><a href="https://localhost:8080/camera">Camera</a></h1>
+      <md-button class="md-raised md-primary" href="https://jlucassr.github.io/ArBC-Pages/">Sobre</md-button>
+      <md-button class="md-raised md-accent" href="https://localhost:8080/camera">Camera</md-button>
     <img alt="Arara" src="../assets/arara.svg"/>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  small {
+    display: block;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'RegularButtons'
+  }
+</script>
