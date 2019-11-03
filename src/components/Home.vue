8a6210b609e3a0a22614fd0567426b3e59da2156
igor-paiva <igorbatistapaiva@outlook.com>
2019-11-02 22:07:56 -0300

    Add some charm and make home cards clickable
    Co-authored-by: Marcelo Victor <marcelovictorg2@gmail.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,6 +17,57 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import ToolBar from '@/components/ToolBar.vue';
+import HomeCard from '@/components/HomeCard.vue';
+
+Vue.component('tool-bar', ToolBar);
+Vue.component('home-card', HomeCard);
+
+@Component({
+  components: {
+    ToolBar,
+    HomeCard,
+  },
+})
+export default class Home extends Vue {
+  private githubCardInfo: object = {
+    name: 'githubCard',
+    icon: 'github',
+    target: '_blank',
+    link: 'https://github.com/fga-eps-mds/2019.2-ArBC',
+    content: 'Para ter acesso ao github, que contém todo o código do produto de forma fácil,\
+     além de muitas outras informações úteis clique aqui.',
+  };
+  private cameraCardInfo: object = {
+    name: 'cameraCard',
+    icon: 'camera_alt',
+    target: '',
+    link: '/camera',
+    content: 'Para ter acesso a câmera do projeto, sendo ela a ferramenta que tem como\
+     função o auxílio na alfabetização de crinças. Clique aqui.',
+  };
+  private wikiCardInfo: object = {
+    name: 'wikiCard',
+    icon: 'help',
+    target: '_blank',
+    link: 'https://jlucassr.github.io/ArBC-Pages/',
+    content: 'Para ter acesso a wiki do projeto, que contém diversas informações úteis sobre os \
+    participantes e o software, clique aqui.',
+  };
+  private driveCardInfo: object = {
+    name: 'driveCard',
+    icon: 'cloud_download',
+    target: '_blank',
+    link: 'https://drive.google.com/open?id=1Aa5jXFIh4385wKT36mFFP82zho7BmfBP',
+    content: 'Para utilizar a aplicação é necessário os marcadores. Desse modo, \
+    disponibilizamos o download por meio do drive. Para baixar basta clicar aqui.',
+  };
+}
+</script>
+
 <style lang="scss" scoped>
   .space-card {
     margin-bottom: 20px;
@@ -40,50 +91,3 @@
   }
 }
 </style>
-
-<script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import ToolBar from '@/components/ToolBar.vue';
-import HomeCard from '@/components/HomeCard.vue';
-
-Vue.component('tool-bar', ToolBar);
-Vue.component('home-card', HomeCard);
-
-@Component({
-  components: {
-    ToolBar,
-    HomeCard,
-  },
-})
-export default class Home extends Vue {
-  private githubCardInfo: object = {
-    icon: 'github',
-    target: '_blank',
-    link: 'https://github.com/fga-eps-mds/2019.2-ArBC',
-    content: 'Para ter acesso ao github, que contém todo o código do produto de forma fácil,\
-     além de muitas outras informações úteis clique no botão azul abaixo.',
-  };
-  private cameraCardInfo: object = {
-    icon: 'camera_alt',
-    target: '',
-    link: '/camera',
-    content: 'Para ter acesso a câmera do projeto, sendo ela a ferramenta que tem como\
-     função o auxílio na alfabetização de crinças.Clique no botão azul abaixo.',
-  };
-  private wikiCardInfo: object = {
-    icon: 'help',
-    target: '_blank',
-    link: 'https://jlucassr.github.io/ArBC-Pages/',
-    content: 'Para ter acesso a wiki do projeto, que contém diversas informações úteis sobre os \
-    participantes e o software, clique no botão azul abaixo.',
-  };
-  private driveCardInfo: object = {
-    icon: 'cloud_download',
-    target: '_blank',
-    link: 'https://drive.google.com/open?id=1Aa5jXFIh4385wKT36mFFP82zho7BmfBP',
-    content: 'Para utilizar a aplicação é necessário os marcadores. Desse modo, \
-    disponibilizamos o download por meio do drive. Para baixar basta clicar no botão azul abaixo.',
-  };
-}
-</script>
--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -1,39 +1,28 @@
 <template>
-  <div class="box">
-    <md-card md-with-hover>
-      <md-ripple>
-        <md-card-header>
-          <md-icon 
-            v-if="!localIcon"
-            class="md-size-4x"
-          >
-            {{ data.icon }}
-          </md-icon>
-          
-          <md-icon
-            v-else
-            class="md-size-4x"
-            :md-src="require('@/assets/github_gray.svg')"
-          />
-        </md-card-header>
+  <md-card :id="data.name" md-with-hover class="box">
+    <md-ripple>
+      <div class="upper-bar" />
 
-        <md-card-content>
-          {{ data.content }}
-        </md-card-content>
+      <md-card-header>
+        <md-icon 
+          v-if="!localIcon"
+          class="md-size-4x"
+        >
+          {{ data.icon }}
+        </md-icon>
+        
+        <md-icon
+          v-else
+          class="md-size-4x"
+          :md-src="require('@/assets/github_gray.svg')"
+        />
+      </md-card-header>
 
-        <md-card-actions>
-          <md-button 
-            class="md-icon-button md-fab md-primary" 
-            :target="data.target"
-            :href="data.link"
-          >
-            <md-icon v-if="!localIcon" class="black">{{ data.icon }}</md-icon>
-            <md-icon v-else :md-src="require(`@/assets/${data.icon}.svg`)"/>
-          </md-button>
-        </md-card-actions>
-      </md-ripple>
-    </md-card>
-  </div>
+      <md-card-content>
+        {{ data.content }}
+      </md-card-content>
+    </md-ripple>
+  </md-card>
 </template>
 
 <script lang="ts">
@@ -53,16 +42,38 @@ import '@/vue_material/components';
     },
   },
 })
-export default class HomeCard extends Vue {}
+export default class HomeCard extends Vue {
+  public mounted() {
+    const card: any = document.getElementById(this.data.name);
+
+    card.addEventListener('click', this.route);
+  }
+  private route() {
+    const { link, target } = this.data;
+
+    if (target === '_blank') {
+      window.open(link, target);
+    } else {
+      window.location.replace(link);
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
   .box {
     width: 320px;
     margin: 4px;
-    height: 300px;
+    height: 250px;
+    border-radius: 5px;
     display: inline-block;
     vertical-align: top;
+  }
+  .upper-bar {
+    width: 100%;
+    height: 5px;
+    border-radius: 5px;
+    background-color: #448aff;
   }
   .md-button {
     background-color: #448aff;
