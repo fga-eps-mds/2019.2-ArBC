400d86c88e78ffa7f77904673f38ae7135d51fc8
Luciano Santos <luciano_z7@hotmail.com>
2019-11-06 23:46:41 -0300

    Dist directory added for build.

---
+++ dist/js/about.f8b6ea43.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{a5a9:function(t,a,e){t.exports=e.p+"img/arara.1489c529.svg"},f820:function(t,a,e){"use strict";e.r(a);var n=function(){var t=this,a=t.$createElement;t._self._c;return t._m(0)},r=[function(){var t=this,a=t.$createElement,n=t._self._c||a;return n("div",{staticClass:"about"},[n("img",{attrs:{alt:"Yoda",src:e("a5a9")}}),n("h1",[t._v("ArBC")])])}],s=e("2877"),c={},u=Object(s["a"])(c,n,r,!1,null,null,null);a["default"]=u.exports}}]);
+//# sourceMappingURL=about.f8b6ea43.js.map
