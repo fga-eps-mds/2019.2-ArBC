a96a1a203194a63f3bfcd6da44aedcc2a40a7c20
Thiago Lopes <thiago.lopes.santos.tls@gmail.com>
2019-11-13 15:38:29 -0300

    Changed cards' color order and refactored  their info texts.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -35,27 +35,27 @@
   export default class Home extends Vue {
     private cameraCardInfo: object = {
       icon: 'camera_alt',
-      iconColor: '#5fad46',
+      iconColor: '#448aff',
       targetBlank: false,
       path: '/camera',
-      content: 'Para abrir a câmera do projeto, sendo ela a ferramenta para\
-        o uso da aplicação na alfabetização de crianças.',
+      content: 'Para acessar a câmera da aplicação, que é usada para fazer \
+       o reconhecimento dos marcadores, clique aqui.',
     };
     private driveCardInfo: object = {
       icon: 'cloud_download',
-      iconColor: '#448aff',
+      iconColor: '#5fad46',
       targetBlank: true,
       path: 'https://drive.google.com/open?id=1Aa5jXFIh4385wKT36mFFP82zho7BmfBP',
-      content: 'Para utilizar a aplicação é necessário os marcadores. Desse modo, \
-        disponibilizamos o download por meio do drive. Para baixar basta clicar aqui.',
+      content: 'O reconhecimento das letras é feito por meio de marcadores, \
+        que estão armazenados no Google Drive. Clique aqui para baixá-los.',
     };
     private wikiCardInfo: object = {
       icon: 'help',
       iconColor: '#f15a22',
       targetBlank: true,
       path: 'https://jlucassr.github.io/ArBC-Pages/',
-      content: 'Para ter acesso a wiki do projeto, que contém diversas \
-        informações úteis sobre os participantes e o software, clique aqui.',
+      content: 'Para acessar a wiki do projeto, que contém diversas \
+        informações adicionais sobre o software, clique aqui.',
     };
   }
 </script>
