1240d2021581e42006c995b3f2be1582ac2a714c
Rhuan Carlos <rhuancarlos.queiroz@gmail.com>
2019-11-27 20:17:49 -0300

    Added missing AMarker mocked component

---
+++ tests/unit/__mocks__/AMarker.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import 'jest';
+  import Component from 'vue-class-component';
+  import { Prop } from 'vue-property-decorator';
+  import { Object3D } from 'three';
+
+  @Component
+  export default class AMarker extends Vue {
+    @Prop(String) public type: string = '';
+    @Prop(String) public url: string = '';
+
+    public object3D = new Object3D();
+  }
+</script>
