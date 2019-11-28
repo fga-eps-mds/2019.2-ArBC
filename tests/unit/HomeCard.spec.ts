29b48d137e695b79d5cc652045eb0bab8ddd0816
Rhuan Carlos <rhuancarlos.queiroz@gmail.com>
2019-11-28 00:59:15 -0300

    Added unit tests to 'canRout' method in 'HomeCard'
    Co-authored-by: igor-paiva <igorbatistapaiva@outlook.com>

--- tests/unit/HomeCard.spec.ts
+++ tests/unit/HomeCard.spec.ts
@@ -6,12 +6,34 @@ let wrapper: any;
 let HomeCard: any;
 
 describe('HomeCard.vue', () => {
-  beforeAll(() => {
-    wrapper = mount(HomeCardComponent);
+  beforeAll(async () => {
+    wrapper = mount(HomeCardComponent, {
+      propsData: {
+        $data: {
+          icon: 'camera_alt',
+          iconColor: '#ff985c',
+          targetBlank: false,
+          path: '/camera',
+          content: 'A camera é a parte mais difícil de testar da aplicação toda, venha',
+        },
+      },
+    });
     HomeCard = wrapper.vm;
   });
 
-  xtest('To do', () => {
-    console.log('HomeCard tests to do');
+  describe('check if route can be changed', () => {
+    test('Invalid path', () => {
+      const spyOnStorageGetItem: any = jest.spyOn(Storage.prototype, 'getItem');
+
+      expect(HomeCard.canRoute('/yo-lo-no-conozco')).resolves.toBeUndefined();
+      expect(spyOnStorageGetItem).toBeCalledWith('showHowToUseDialog');
+    });
+
+    test('Valid path', () => {
+      const spyOnStorageGetItem: any = jest.spyOn(Storage.prototype, 'getItem');
+
+      expect(HomeCard.canRoute('/camera')).rejects.toBeUndefined();
+      expect(spyOnStorageGetItem).toBeCalledWith('showHowToUseDialog');
+    });
   });
 });
